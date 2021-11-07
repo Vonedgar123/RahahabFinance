8e9543c5f98e70ffa7883a3a93287b92c6486fe0
IchinoseKaedeLucisCaelum <michaelchen1393@gmail.com>
2021-11-07 17:26:05 +0700

    Login Ditambah, Web.php dan Controllernya diubah

--- app/Http/Controllers/RFController.php
+++ app/Http/Controllers/RFController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class RFController extends Controller
 {
-    public function tampil()
+    public function login()
     {
-        $result = DB::select("select * from pokemons");
+        $result = DB::select("select * from Pengguna");
         return view('home', ['pokemons' => $result]);
     }
 }
